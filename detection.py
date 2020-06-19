21311b8540d45d7f08c13a10c2cbe27c9f0ca6bc
Rahul Malik <ramalik00@gmail.com>
2020-06-19 21:56:32 +0530

    f

---
+++ detection.py
@@ -0,0 +1,47 @@
+import numpy as np
+import cv2
+
+Min_Conf=0.3
+NMS_THRESH=0.3
+def detect_people(frame, net, ln):
+	(H, W) = frame.shape[:2]
+	results = []
+	blob = cv2.dnn.blobFromImage(frame, 1 / 255.0, (416, 416),
+		swapRB=True, crop=False)
+	net.setInput(blob)
+	layerOutputs = net.forward(ln)
+	boxes = []
+	centroids = []
+	confidences = []
+	classes=[]
+
+	for output in layerOutputs:
+		for detection in output:
+			scores = detection[5:]
+			classID = np.argmax(scores)
+			confidence = scores[classID]
+			if confidence > Min_Conf:
+                                
+                                box = detection[0:4] * np.array([W, H, W, H])
+                                (X_center,Y_center,w,h)=box.astype("int")
+                                x=int(X_center-(w/2))
+                                y=int(Y_center-(w/2))
+                                boxes.append([x, y, int(w), int(h)])
+                                centroids.append((X_center,Y_center))
+                                confidences.append(float(confidence))
+                                classes.append(classID)
+				
+
+	indexs = cv2.dnn.NMSBoxes(boxes, confidences, Min_Conf, NMS_THRESH)
+
+	
+	if len(indexs) > 0:
+		
+		for i in indexs.flatten():
+			(x,y) = (boxes[i][0], boxes[i][1])
+			(w,h) = (boxes[i][2], boxes[i][3])
+			r = (confidences[i], (x, y, x + w, y + h), centroids[i],classes[i])
+			results.append(r)
+
+	
+	return results
---
+++ object_detector_image.py
@@ -0,0 +1,57 @@
+
+from detection import detect_people
+import numpy as np
+import argparse
+import cv2
+import imutils
+import os
+USE_GPU=False
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--input", type=str, default="0")
+ap.add_argument("-o", "--output", type=str, default="")
+ap.add_argument("-d", "--display", type=int, default=1)
+args = vars(ap.parse_args())
+
+labelsPath = os.path.sep.join(["yolo-coco", "coco.names"])
+LABELS = open(labelsPath).read().strip().split("\n")
+
+weightsPath = os.path.sep.join(["yolo-coco", "yolov3.weights"])
+configPath = os.path.sep.join(["yolo-coco", "yolov3.cfg"])
+
+print("MODEL LOADED")
+net = cv2.dnn.readNetFromDarknet(configPath, weightsPath)
+
+if USE_GPU:
+	net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
+	net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
+
+
+last_layer = net.getLayerNames()
+last_layer = [last_layer[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+
+frame = cv2.imread(args["input"])
+frame = imutils.resize(frame, width=800)
+results = detect_people(frame, net, last_layer)
+
+for (i, (prob, bounding_box, centroid,classes)) in enumerate(results):
+        
+	X_start, Y_start, X_end, Y_end = bounding_box	
+	cv2.rectangle(frame,(X_start, Y_start),(X_end, Y_end),(0,255,0), 2)
+	text=LABELS[classes]
+	cv2.putText(frame,text,(X_start,Y_start-10),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+
+
+text = "Total Objects: {}".format(len(results))
+cv2.putText(frame, text, (10, frame.shape[0] - 25),cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 3)		
+	
+if args["display"] > 0:
+		
+	cv2.imshow("Frame", frame)
+	cv2.waitKey(0)
+	cv2.destroyAllWindows()
+
+		
+if args["output"] != "":
+        cv2.imwrite(args["output"],frame)
+	
